c79b236dbd179ad2fbc5d7998dbc2fd094902031
Vinodh G <33771537+gvinodh1@users.noreply.github.com>
2022-10-22 14:49:55 -0700

    Version 1.0

---
+++ check_configuration_manager_client.ps1
@@ -0,0 +1,25 @@
+﻿<#
+  Name: check_configuration_manager_client
+  Version: 1.0
+  Created: October, 2022
+  Created By: Vinodh G gvinodh@vmware.com
+
+  Description: The Script returns the installation status of Configuration Manager Client
+
+  Execution Context: System
+  Execution Architecture: Auto
+  Response Type: String
+#>
+
+##############################################################################
+
+$app = Get-WmiObject -Class Win32_Product | where name -EQ "configuration manager client" | where vendor -EQ "Microsoft Corporation" 
+if ($app.name -ne $null)
+{
+$app.name
+}
+else
+{
+$Output = "Configuration Manager Client is Not Installed"
+$Output
+}
