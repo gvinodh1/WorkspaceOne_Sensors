b7475c6c2a34ddad79458ec650d18e1d8f66b245
Vinodh G <33771537+gvinodh1@users.noreply.github.com>
2022-10-23 09:17:09 -0700

    The Script returns the status of USB on the end device (Enabled or Disabled)

---
+++ get_usb_status.ps1
@@ -0,0 +1,26 @@
+﻿<#
+  Name: get_usb_status
+  Version: 1.0
+  Created: September, 2022
+  Created By: Vinodh G gvinodh@vmware.com
+
+  Description: The Script returns the status of USB on the end device (Enabled or Disabled)
+
+  Execution Context: System
+  Execution Architecture: Auto
+  Response Type: String
+#>
+
+##############################################################################
+
+
+$UsbStatus=Get-ItemProperty -path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\RemovableStorageDevices" -ErrorAction SilentlyContinue
+if($UsbStatus.Deny_All -eq 1)
+{
+	$Output="Disabled"
+}
+else
+{
+	$Output="Enabled"
+}
+$Output
