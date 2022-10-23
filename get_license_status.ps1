e789a77dabcfb013dba241e7e861df125b40325d
Vinodh G <33771537+gvinodh1@users.noreply.github.com>
2022-10-23 09:36:38 -0700

    The Script returns the Windows license status.

---
+++ get_license_status.ps1
@@ -0,0 +1,17 @@
+﻿<#
+  Name: get_license_status
+  Version: 1.0
+  Created: August, 2022
+  Created By: Vinodh G gvinodh@vmware.com
+
+  Description: The Script returns the Windows license status. We used this script for a customer to get the KMS Activation Status.
+
+  Execution Context: System
+  Execution Architecture: Auto
+  Response Type: String
+#>
+
+######################################################################################################################
+
+$KMSActivationStatus=Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" | where { $_.PartialProductKey } | select Description, LicenseStatus
+$KMSActivationStatus.Description
