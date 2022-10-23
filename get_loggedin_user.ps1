a591201e08acf128eaea9c72ef333886741b1384
Vinodh G <33771537+gvinodh1@users.noreply.github.com>
2022-10-23 09:26:41 -0700

    The Script returns the current loggedin User on the machine.

---
+++ get_loggedin_user.ps1
@@ -0,0 +1,17 @@
+﻿<#
+  Name: get_loggedin_user
+  Version: 1.0
+  Created: August, 2022
+  Created By: Vinodh G gvinodh@vmware.com
+
+  Description: The Script returns the current loggedin User on the machine.
+
+  Execution Context: System
+  Execution Architecture: Auto
+  Response Type: String
+#>
+
+######################################################################################################################
+
+$current_user = Get-WMIObject -class Win32_ComputerSystem | select username
+$current_user.username
